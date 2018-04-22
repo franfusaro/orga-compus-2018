deddcae0efccedd4157a5c12b934eb65a77f3661
rocioimpa <rocio.impaglione@gmail.com>
2018-04-22 17:46:50 -0300

    Se modifico archivo .h con nueva funcion

--- tp1-2018/mips32_plot.h
+++ tp1-2018/mips32_plot.h
@@ -1,4 +1,5 @@
 #include <param.h>
 
-extern void calculo_brillo(float cr, float ci);
+extern void calculo_brillo(float cr, float ci, int x, int y);
+extern void for_loops(float params->UL_im,float params->UL_re,float params->d_re,float params->d_im,int params->x_res,int params->y_res)
 extern void mips32_plot(params_t *parms);
