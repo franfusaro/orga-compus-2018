a3969f4f84ecf49544d972e573c4a9afdea33c11
raknarok8 <franco_094@hotmail.com>
2018-05-02 20:40:27 -0300

    solo falta la parte de verificacion de errores de sys_write

--- tp1-2018/mips32_plot.h
+++ tp1-2018/mips32_plot.h
@@ -3,3 +3,4 @@
 extern void calculo_brillo(params_t *parms,float cr, float ci);
 extern void for_loops(params_t *parms)
 extern void mips32_plot(params_t *parms);
+extern void mips32_plot_c(params_t *parms);
